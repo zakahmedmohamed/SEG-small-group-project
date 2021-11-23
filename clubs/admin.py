3c23bf253d2d09f36c51c6d89fc37a4cd65ded21
Ammar Alshareef <ammar.alsharef@kcl.ac.uk>
2021-11-23 14:51:42 +0300

    Add seed and unseed command
    Create seed command that creates fake users.
    seed.py might need refactoring later.

--- clubs/admin.py
+++ clubs/admin.py
@@ -1,3 +1,11 @@
+"""Congifuration of the adminstrative interface for clubs."""
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Configuration of the admin interface for users."""
+    list_display = [
+        'username', 'first_name', 'last_name', 'email', 'chess_xp',
+        'is_member', 'is_owner', 'is_officer'
+    ]
--- clubs/management/commands/unseed.py
+++ clubs/management/commands/unseed.py
@@ -1,7 +1,17 @@
 from django.core.management.base import BaseCommand, CommandError
+from clubs.models import User
 
 class Command(BaseCommand):
         """The database unseeder."""
+        def __init__(self):
+            super().__init__()
 
         def handle(self, *args, **options):
-            print("TODO: The database unseeder will be added here...")
+            #print("TODO: The database unseeder will be added here...")
+            user_set = User.objects.all()
+
+            print('unseeding data...')
+            for user in user_set.iterator():
+                if user.is_superuser == False:
+                    user.delete()
+            print('done.')
