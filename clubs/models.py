8c3fa3f891dd57056308c78041e95a4c056fdba7
Zakariya Ahmed Mohamed <zakariya.ahmed_mohamed@kcl.ac.uk>
2021-12-09 16:22:08 +0000

    resolve merge conflicts

--- clubs/models.py
+++ clubs/models.py
@@ -23,10 +23,7 @@ class User(AbstractUser):
     bio = models.CharField(max_length=520, blank=True)
     statement = models.CharField(max_length=1000, blank=False)
     chess_xp = models.IntegerField(validators = [MinValueValidator(0)], default=0)
-<<<<<<< HEAD
     
-=======
->>>>>>> e27fe9a4e1f4781a7d316a7b0dc42bee25c706fa
 
     def full_name(self):
         return f'{self.first_name} {self.last_name}'
