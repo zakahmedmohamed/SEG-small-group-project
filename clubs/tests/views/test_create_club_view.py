ad3fc5be04d000093da19d57a87a116231eb08ba
Rohit Pun <rohit.pun@kcl.ac.uk>
2021-12-10 17:02:33 +0000

    Add owner commands test

--- clubs/tests/views/test_create_club_view.py
+++ clubs/tests/views/test_create_club_view.py
@@ -1,43 +1,32 @@
+"""Referenced from clucker application"""
+"""Tests of the sign up view."""
 from django.contrib.auth.hashers import check_password
-from django.http import response
 from django.test import TestCase
 from django.urls import reverse
-from clubs.forms import Create_A_Club_Form
-from clubs.models import User, Club
+from clubs.forms import SignUpForm, Create_A_Club_Form
+from clubs.models import User, Club, UserClubs
 from clubs.tests.helpers import LogInTester
 
+class SignUpViewTestCase(TestCase, LogInTester):
 
-class createCLubTestCase(TestCase):
+    fixtures = ["clubs/tests/fixtures/users.json"]
 
+    """Tests of the sign up view."""
     def setUp(self):
         self.url = reverse('create_club')
-        self.forminput = {'name': 'test club','description': 'test description', 'location': 'london'}
+        self.user = User.objects.get(username='janedoe@example.org')
+        self.client.login(username=self.user.username, password='Password123')
         self.form_input = {
-            'first_name': 'Jane',
-            'last_name': 'Doe',
-            'username': 'janedoe@example.org',
-            'bio': 'My bio',
-            'statement': 'My statement',
-            'chess_xp': 100,
-            'new_password': 'Password123',
-            'password_confirmation': 'Password123'
+            'name': 'Club B',
+            'description': 'This is Club B',
+            'location': 'London',
         }
 
-    def test_create_club_url(self):
-        self.assertEquals(self.url, '/create_club/')
+    def test_sign_up_url(self):
+        self.assertEqual(self.url,'/create_club/')
 
-    def test_create_a_club_success(self):
-        self.client.login(username= 'johndoe@example.org', password = 'Password123')
-        response = self.client.post(self.url, self.form_input, follow=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'create_club.html')
-        form = response.context['form']
-        self.assertTrue(isinstance(form, Create_A_Club_Form))
-
-    def test_create_a_club_fail(self):
-        self.client.login(username= 'johndoe@example.org', password = 'WrongPassword123')
-        self.form_input['name'] = ""
-        response = self.client.post(self.url, self.form_input, follow=True)
+    def test_get_create_club(self):
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'create_club.html')
         form = response.context['form']
@@ -56,7 +45,7 @@ class createCLubTestCase(TestCase):
         self.assertTrue(isinstance(form, Create_A_Club_Form))
         self.assertTrue(form.is_bound)
 
-    def test_succesful_create_club(self):
+    def test_succesful_sign_up(self):
         before_count = Club.objects.count()
         response = self.client.post(self.url, self.form_input, follow=True)
         after_count = Club.objects.count()
