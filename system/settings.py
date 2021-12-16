92e41dc50d33a978006d6e9814a6c9721d4e734c
Zakariya Ahmed Mohamed <zakariya.ahmed_mohamed@kcl.ac.uk>
2021-12-16 16:52:38 +0000

    Configure app for deployment

--- system/settings.py
+++ system/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -134,3 +136,6 @@ AUTH_USERCLUBS_MODEL = 'clubs.UserClubs'
 
 # Login URL for redirecting users from login protected views
 LOGIN_URL = 'log_in'
+
+#Activate django_heroku
+django_heroku.settings(locals())
